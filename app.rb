0c212cee82a1b71cf3edbd92ce2f13e0e1ad4556
Lara Sanders <larasanders@gmail.com>
2017-12-30 17:56:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
-    @session == session
-    Item: <input type = "text" name="item" >
+    @session == session[:foo] == 'hello'
 
     if session[:foo] == 'hello'
 
